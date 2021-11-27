b1231988cc8d3c7258aa79593b5e704c52aea590
QasemMh <kasem200094@gmail.com>
2021-11-27 04:20:45 +0200

    commit 3

--- js/app.js
+++ js/app.js
@@ -15,9 +15,51 @@ optionIcon.addEventListener("click", () => {
   }
 });
 
+//setting
+
+const navStyle = document.querySelector(".navbar-style");
+const navbar_1 = document.querySelector(".navbar-1");
+const navbar_2 = document.querySelector(".navbar-2");
+const navbar_3 = document.querySelector(".navbar-3");
+const main = document.querySelector("main");
+
+const footer_style = document.querySelector(".footer-style");
+const footer = document.querySelector(".wrapper");
+
+navStyle.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-1")) {
+    navbar_1.style.display = "block";
+    navbar_2.style.display = "none";
+    navbar_3.style.display = "none";
+    main.classList.add("main");
+  }
+  if (e.target.classList.contains("btn-2")) {
+    navbar_2.style.display = "flex";
+    navbar_1.style.display = "none";
+    navbar_3.style.display = "none";
+    main.classList.remove("main");
+  }
+  if (e.target.classList.contains("btn-3")) {
+    navbar_1.style.display = "none";
+    navbar_2.style.display = "none";
+    navbar_3.style.display = "flex";
+    main.classList.remove("main");
+  }
+});
+
+footer_style.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-1")) {
+    footer.classList.remove("tow");
+  }
+  if (e.target.classList.contains("btn-2")) {
+    footer.classList.add("tow");
+  }
+});
+//todo save setting to localhost
+
 /*options */
 
-/* navbar 1 */
+/* theme 1 */
 const theme = document.querySelector("#themeButton");
 
 let index = 1;
@@ -41,27 +83,17 @@ if (localStorage.getItem("index") != null) {
   index++;
   if (index > color.length - 1) index = 0;
 }
-/* end navbar 1 */
+// end theme
 
-/* navbar 2 */
+/* navbar 2  - 3*/
 const menu = document.querySelector(".navbar-2 .menu");
 const navLinks = document.querySelector(".navbar-2 .navbar-nav");
 menu.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
-/* end navbar 2 */
-
-const styleNav = document.querySelector(".navbar-style");
-
-styleNav.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn-1")) {
-    document.querySelector(".navbar-1").style.display = "block";
-    document.querySelector(".navbar-2").style.display = "none";
-    // document.querySelector(".navbar-3").style.display = "none";
-  }
-  if (e.target.classList.contains("btn-2")) {
-    document.querySelector(".navbar-2").style.display = "flex";
-    document.querySelector(".navbar-1").style.display = "none";
-    //document.querySelector(".navbar-3").style.display = "none";
-  }
+const menu2 = document.querySelector(".navbar-3 .menu");
+const navLinks2 = document.querySelector(".navbar-3 .navbar-nav");
+menu2.addEventListener("click", () => {
+  navLinks2.classList.toggle("open");
 });
+/* end navbar 2 - 3 */
